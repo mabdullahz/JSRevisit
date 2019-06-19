49ed53fd05be7835688bed94509a8c9d5090e993
Abdullah Zafar <abdullah@A002-00080.arbisoft.com>
2019-06-19 12:51:41 +0500

    inheritance snippet added

---
+++ Inheritance.js
@@ -0,0 +1,30 @@
+/* Inheritance */
+
+function SuperClass () {
+  this.name = 'SuperClass'
+}
+
+SuperClass.prototype.getName = function () {
+  return this.name
+}
+
+function SubClass () {
+  this.type = 1
+}
+
+/* A property defined at this point will be overwritten */
+// SubClass.prototype.getType = function () {
+//   return this.type
+// }
+
+SubClass.prototype = new SuperClass()
+
+SubClass.prototype.getType = function () {
+  return this.type
+}
+
+var obj = new SubClass()
+console.log(obj.name)
+console.log(obj.type)
+console.log(obj.getName())
+console.log(obj.getType())
