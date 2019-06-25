37f165a85e87ad0baca7fb0d386fd9023810287b
Abdullah Zafar <abdullah@A002-00080.arbisoft.com>
2019-06-25 13:25:21 +0500

    Promise.add and Promise.race added

--- Promise-Chaining.js
+++ Promise-Chaining.js
@@ -46,3 +46,19 @@ delayer(1000, 0)
     console.log('Hello, ' + num + '!')
     return delayer(1000, num)
   })
+
+// USING Promise.all()
+// completes after 3.5 seconds as per the second Promise
+Promise.all([
+  delayer(1000, 0),
+  delayer(3500, 2.5),
+  delayer(500, -0.5)
+]).then((arr) => console.log(arr))
+
+// USING Promise.race()
+// completes after 500ms as per the third Promise
+Promise.race([
+  delayer(1000, 0),
+  delayer(3500, 2.5),
+  delayer(500, -0.5)
+]).then((arr) => console.log(arr))
